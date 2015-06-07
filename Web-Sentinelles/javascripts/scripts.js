c69a5b59c8f6de1ab3704e3d5045f8d168598d12
Hougo13 <hougo13@gmail.com>
2015-06-07 16:55:52 +0200

    Anim

--- Web-Sentinelles/javascripts/scripts.js
+++ Web-Sentinelles/javascripts/scripts.js
@@ -1,7 +1,17 @@
 $(function(){
+    var drop1 = 0;
+    var drop2 = 0;
     $('.dropmenu').hover(function(){
         $('.navbar-footer').addClass('dropped');
+        $('.container').addClass('dropped');
     }, function(){
-        $('.navbar-footer').removeClass('dropped');
+        $('.navbar-nav li').not('.dropmenu').hover(function(){
+            $('.navbar-footer').removeClass('dropped');
+            $('.container').removeClass('dropped');
+        });
+        $('.navbar-footer').mouseleave(function(){
+            $('.navbar-footer').removeClass('dropped');
+            $('.container').removeClass('dropped');
+        });
     });
 });
